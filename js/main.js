e242e226b3bf57bd6bbf9bbb33aaeba369f28125
anna <nyufffka@gmail.com>
2020-01-26 20:59:05 +0200

    markup update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,40 @@
 
-// Header fade
-jQuery(document).ready(function($) {
+// Header-footer fade
 
-	$(function() {
-		$(window).scroll(function() {
-			if ($(window).scrollTop() != 0) {
-				$('.header').css({"background-color": "rgba(222, 222, 210, 1)", "transition-duration": "1s"});
-			}
-			else {
-				$('.header').css({"background-color": "rgba(222, 222, 210, 0)", "transition-duration": "1s"})
-			}
-		});
+	jQuery(document).ready(function($) {
 
-		$('.header').hover(
-			function() {
-				if ($(window).scrollTop() == 0) {
+		$(function() {
+			$(main).scroll(function() {
+				if ($(main).scrollTop() != 0) {
 					$('.header').css({"background-color": "rgba(222, 222, 210, 1)", "transition-duration": "1s"});
+					$('.hide').css({"display": "block"});
+                    $('.footer').css({"display": "none"});
 				}
-			},
-			function() {
-				if ($(window).scrollTop() == 0) {
-					$('.header').css({"background-color": "rgba(222, 222, 210, 0)", "transition-duration": "1s"})
+				else {
+					$('.header').css({"background-color": "rgba(222, 222, 210, 0)", "transition-duration": "1s"});
+					$('.footer').css({"display": "flex"});
+                    $('.hide').css({"display": "none"});
 				}
 			});
+	
+			$('.header').hover(
+				function() {
+					if ($(window).scrollTop() == 0) {
+						$('.header').css({"background-color": "rgba(222, 222, 210, 1)", "transition-duration": "1s"});
+					}
+				},
+				function() {
+					if ($(main).scrollTop() == 0) {
+						$('.header').css({"background-color": "rgba(222, 222, 210, 0)", "transition-duration": "1s"})
+					}
+				});
+				$('.hide').click(
+					function() {
+						$('.footer').css({"display": "flex"});
+						$('.hide').css({"display": "none"});
+					});
+			});	
 	});
-});
-
 	
 
 window.onload = function() {
@@ -44,36 +53,16 @@ window.onload = function() {
 	        	} catch (e) {
 	        		//do nothing
 	        	}
-
 	        } 
 	    document.getElementById('main').innerHTML = content;
 	    });
 	});
 
 	// Fill templates with property
-	showProp();
+
+	// Fill reviews
+
 }
 
-function showProp() {
-    let prop = JSON.parse(window.localStorage.getItem('houses'));
-	let propTempl = document.getElementById('propTempl');
-    for (let item in prop) {
-		let elem = document.createElement('div');
-        elem.classList.add("container");
-        elem.classList.add("container-prop");
-        let parent = document.querySelector('.prop');
-        elem.append(propTempl.content.cloneNode(true));
-		parent.append(elem);
-			document.querySelectorAll(".address")[i].innerHTML = prop[item].address;
-			//document.querySelectorAll(".zip")[i].innerHTML = prop[item].itemszip;
-			document.querySelectorAll(".floors")[i].innerHTML = prop[item].floors;
-			document.querySelectorAll(".bedrooms")[i].innerHTML = prop[item].bedrooms;
-			document.querySelectorAll(".bathrooms")[i].innerHTML = prop[item].bathrooms;
-			//document.querySelectorAll(".area")[i].innerHTML = prop[item].area;
-			document.querySelectorAll(".price")[i].innerHTML = prop[item].price;
-			document.querySelectorAll(".year")[i].innerHTML = prop[item].yearbuilt;
-			document.querySelectorAll(".descr")[i].innerHTML = prop[item].description;
-		}
-	}
 
 	
